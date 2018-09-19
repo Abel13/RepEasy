3e88511e2f23e3039f6f7ffb2b83a07926d340b7
DESKTOP-OSHJEMM\abelo <abel.o.d@outlook.com>
2018-09-19 18:56:23 -0300

    - Criação da tabela Despesa - Criação da tabela DespesaMorador

---
+++ RepEasyDesktop/RepEasyDatabase/Despesa.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Despesa]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Descricao] NCHAR(50) NOT NULL, 
+    [Valor] NUMERIC NULL,
+
+)
---
+++ RepEasyDesktop/RepEasyDatabase/DespesaMorador.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[DespesaMorador]
+(
+  [Morador] INT NOT NULL,
+  [Despesa] INT NOT NULL,
+  PRIMARY KEY CLUSTERED ([Despesa], [Despesa]),
+  FOREIGN KEY ([Morador]) REFERENCES [dbo].[Morador] ([Id]) ON DELETE CASCADE ON UPDATE NO ACTION,
+  FOREIGN KEY ([Despesa]) REFERENCES [dbo].[Despesa] ([Id]) ON DELETE CASCADE ON UPDATE NO ACTION
+)
