d60b1b2a5f56f8f3d8946deac2710e23853ae934
phelipeW <33735268+phelipeW@users.noreply.github.com>
2018-10-04 20:10:32 -0300

    criaçao da classe Sessao e UserControlPerfil

---
+++ RepEasyDesktop/RepEasyDesktop/Model/Sessao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepEasyDesktop.Model
+{
+    public class Sessao
+    {
+        public static Sessao instancia;
+        public Morador Morador { get; private set; }
+
+
+        private Sessao()
+        {
+
+        }
+
+      
+        public static Sessao GetInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new Sessao();
+                  
+            }
+
+            return instancia;
+        }
+    }
+}
