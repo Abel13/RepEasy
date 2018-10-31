7da28a570bd817f6f7d540b6fac41ab26f1ef8aa
abel13 <abel.o.d@outlook.com>
2018-10-30 21:05:21 -0300

    - configuração azure e cadastro despesa

--- RepEasyDesktop/RepEasyDesktop/View/UserControlNovaDespesa.xaml.cs
+++ RepEasyDesktop/RepEasyDesktop/View/UserControlNovaDespesa.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using RepEasyDesktop.Control;
+using RepEasyDesktop.View.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,34 @@ namespace RepEasyDesktop.View
     /// </summary>
     public partial class UserControlNovaDespesa : UserControl
     {
+        ControlNovaDespesa control;
+        ObservableCollection<ViewModelItemCompra> itensCompra;
+
         public UserControlNovaDespesa()
         {
             InitializeComponent();
+
+            control = new ControlNovaDespesa();
+            itensCompra = new ObservableCollection<ViewModelItemCompra>();
+
+            ListViewItens.ItemsSource = itensCompra;
+            ListViewParticipantes.ItemsSource = control.CarregarParticipantes();
+        }
+
+        private void ButtonIncluirItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(TextBoxItem.Text))
+            {
+                itensCompra.Add(new ViewModelItemCompra { Descricao = TextBoxItem.Text });
+                TextBoxItem.Text = string.Empty;
+                TextBoxItem.Focus();
+            }
+        }
+        
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = (ViewModelItemCompra)((Button)sender).DataContext;
+            itensCompra.Remove(vm);
         }
     }
 }
---
+++ RepEasyDesktop/RepEasyDesktop/View/ViewModel/ViewModelItemCompra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepEasyDesktop.View.ViewModel
+{
+    public class ViewModelItemCompra
+    {
+        
+        public string Descricao { get; set; }
+    }
+}
