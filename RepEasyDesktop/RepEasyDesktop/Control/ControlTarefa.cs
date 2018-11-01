fceeec2f366e08c02a1a33433639088181fc6ca9
gardheamsantos <gardheam.santos@yahoo.com.br>
2018-11-01 11:05:24 -0300

    cadastro de  tarefa

--- RepEasyDesktop/RepEasyDesktop/Control/ControlTarefa.cs
+++ RepEasyDesktop/RepEasyDesktop/Control/ControlTarefa.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RepEasyDesktop.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,34 @@ using System.Threading.Tasks;
 
 namespace RepEasyDesktop.Control
 {
+
     class ControlTarefa
     {
+        Model.Model context = new Model.Model();
+
+        public bool CadastrarTarefa(string titulo, string descricao, DateTime data,List<object> morador)
+        {
+            List<Morador> moradores= new List<Morador>();
+            foreach (var item in morador)
+            {
+                moradores.Add((Morador)item);
+            }
+
+            Tarefa tarefa = new Tarefa() { Titulo = titulo, Descricao = descricao ,Data=data,Morador = moradores};
+            context.Tarefas.Add(tarefa);
+
+            return context.SaveChanges() > 0;
+        }
+
+
+        public List<Morador> ListarMoradores()
+        {
+            var listaMoradores = (from m in context.Moradores
+                                  select m).ToList();
+
+            return listaMoradores;
+        }
     }
+
+
 }
--- RepEasyDesktop/RepEasyDesktop/View/UserControlTarefa.xaml.cs
+++ RepEasyDesktop/RepEasyDesktop/View/UserControlTarefa.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using RepEasyDesktop.Control;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +21,65 @@ namespace RepEasyDesktop.View
     /// </summary>
     public partial class UserControlTarefa : UserControl
     {
+        ControlTarefa controlTarefa;
+        List<object> moradores;
+
         public UserControlTarefa()
         {
             InitializeComponent();
+            controlTarefa = new ControlTarefa();
+            ComboBoxListaMorador.ItemsSource = controlTarefa.ListarMoradores();
+            moradores = new List<object>();
+
         }
 
         private void ButtonSalvarTarefa_Click(object sender, RoutedEventArgs e)
         {
+            var messageQueue = SnackbarThree.MessageQueue;
+            string titulo = TexBoxTituloTarefa.Text;
+            string descricao = TextBoxDescricaoTarefa.Text;
+            DateTime data = TextDataTarefa.SelectedDate.Value;
+            
 
+
+
+            if (String.IsNullOrEmpty(titulo))
+            {
+                TexBoxTituloTarefa.Focus();
+                Task.Factory.StartNew(() => messageQueue.Enqueue("Titulo da tarefa não informado."));
+                return;
+            }
+            if (String.IsNullOrEmpty(descricao))
+            {
+                TextBoxDescricaoTarefa.Focus();
+                Task.Factory.StartNew(() => messageQueue.Enqueue("Descrição da tarefa não informada."));
+                return;
+            }
+            if (TextDataTarefa.SelectedDate == null)
+            {
+                TextDataTarefa.Focus();
+                Task.Factory.StartNew(() => messageQueue.Enqueue("Data da tarefa não informada."));
+                return;
+            } 
+
+            if( !(controlTarefa.CadastrarTarefa(titulo, descricao, data, moradores)))
+            {
+                Task.Factory.StartNew(() => messageQueue.Enqueue("Erro no cadastro, tente novamente!"));
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataGridTabelaTarefas.ItemsSource = controlTarefa.ListarMoradores();
+        }
+
+        private void select_check_morador(object sender, RoutedEventArgs e)
+        {
+            bool participa = ((CheckBox)sender).IsChecked ?? false;
+            if (participa)
+                moradores.Add(((CheckBox)sender).DataContext);
+            else
+                moradores.Remove(((CheckBox)sender).DataContext);
         }
     }
 }
